0dd50691f7bac44aaa398aae6338537b429e5ce9
tsubasa-0512 <61370253+tsubasa-0512@users.noreply.github.com>
2020-12-13 17:54:25 +0900

    課題提出
    JavaScript1-6課題提出

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,19 @@
+// 問1
+let scores = [10, 15, 20, 25];
+
+for (let i = 0; i < scores.length; i++) {
+    if(scores[i] % 2 === 0){
+        console.log(scores[i] + "は偶数です");
+    }else{
+        continue
+    }
+}
+
+// 問2
+let car = {
+    gass: 20.5,
+    num: 1234,
+};
+
+console.log(`ガソリンは${car.gass}です`);
+console.log(`ナンバーは${car.num}です`);
